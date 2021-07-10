f0fbbe6953026ef305b8710fb27237bd6a6278a3
JP Dillingham <jpdillingham@gmail.com>
2021-07-10 09:11:27 -0500

    add customers lib

---
+++ src/lib/customers.js
@@ -0,0 +1,19 @@
+let customers = [{
+  id: abc123,
+  name: 'jane doe',
+  address: '123 fake street, anytown USA'
+}];
+
+const get = () => customers;
+
+const add = ({ customer }) => customers.push(customer);
+
+const remove = ({ id }) => {
+  customers = customers.filter(customer => customer.id !== id);
+};
+
+module.exports = {
+  get,
+  add,
+  remove
+};
