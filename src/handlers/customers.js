f4f82b4026243f548dfdad783d5be084c802b9ed
JP Dillingham <jpdillingham@gmail.com>
2021-07-17 12:25:44 -0500

    add metrics, tweak logging

--- src/handlers/customers.js
+++ src/handlers/customers.js
@@ -1,21 +1,61 @@
+const { omit, pick } = require('lodash');
 const serverless = require("serverless-http");
+const { metricScope } = require("aws-embedded-metrics");
 const express = require("express");
-const bodyParser = require('body-parser');
 
 const customers = require('../lib/customers');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use((req, res, next) => {
+app.use(metricScope(metrics => async (req, res, next) => {
+  const start = Date.now();
+
+  metrics.setNamespace("ServerlessExpress");
+  metrics.setProperty("Path", req.path);
+  metrics.setProperty("Method", req.method);
+  metrics.setProperty("RequestId", req.headers["x-request-id"])
+  req['metrics'] = metrics;
+  
   const { apiGateway: { event }} = req;
-  console.log(JSON.stringify(event));
+  const { headers: requestHeaders, httpMethod, path, pathParameters, queryStringParameters, requestContext, resource, stageVariables } = event;
+  
   next();
-});
+
+  const { statusCode, statusMessage, _header: responseHeaders } = res;
+  const duration = Date.now() - start;
+  
+  console.log(JSON.stringify({
+    request: {
+      httpMethod, 
+      path,
+      pathParameters, 
+      queryStringParameters, 
+      requestHeaders, 
+      context: {
+        requestContext, 
+        resource, 
+        stageVariables
+      }
+    },
+    response: {
+      statusCode, 
+      statusMessage, 
+      responseHeaders
+    },
+    duration
+  }));
+
+  req.metrics.putMetric("HandlerTime", duration);
+}));
 
 app.get("/customers", (req, res) => {
+  const records = customers.get();
+
+  req.metrics.putMetric("CustomerCount", (records || []).length);
+
   return res
-    .status(200).json(customers.get());
+    .status(200).json(records);
 });
 
 app.get("/customers/:id", (req, res) => {
